a220754222d948792cb8497578abe0499941fb4b
Arif039 <saif039@gmail.com>
2025-03-11 14:32:48 +0600

    contact button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -55,7 +55,7 @@ function Contact () {
                                 
                             </div>
 
-                            <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer">Submit</button>
+                            <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white  md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer">Submit</button>
 
                         </form>
                     </div>
